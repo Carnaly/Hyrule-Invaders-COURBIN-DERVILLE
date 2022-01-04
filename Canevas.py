19b4bc15f45102e69565c4e5c4638b7da322f0b5
Carnaly <91116199+Carnaly@users.noreply.github.com>
2022-01-04 09:12:37 +0100

    Ajout des images de coeur + Canvas Menu + LogoMenu

--- Canevas.py
+++ Canevas.py
@@ -13,7 +13,8 @@ Que reste-t-il à faire ?
     -Bouton "New game"
     -Bouton "Rage quit"
 """
-from tkinter import Button, Canvas, Label, Tk, mainloop
+from tkinter import Button, Canvas, Label, PhotoImage, Tk, mainloop
+from tkinter.constants import ANCHOR, NW
 
 
 #Création de la fenêtre
@@ -25,18 +26,28 @@ invade.geometry('1920x1080')
 Canevas = Canvas(invade, width=1300, height=800, bg='black')
 Canevas.place(x=0, y=0)
 
+#Menu à droite
+Menu = Canvas(invade, width=250, height=800, bg='white')
+Menu.place(x=1300, y=0)
+
+#Logo Menu
+imgname=PhotoImage(file="Logopetit.png")
+Menu.create_image(25,0, anchor=NW, image=imgname)
+Menu.plop=imgname
+
+
 #Zone de Score
 Score= Label(invade, text="Score")
 Score.config(font=('Courier',12))
-Score.place (x=1400, y = 100)
+Score.place (in_=Menu, x=100, y = 250)
 
 #bouton quitter
 Quit = Button(invade, text = 'Quitter', command = invade.destroy)
-Quit.place(x=1400, y= 400)
+Quit.place(in_=Menu, x=100, y= 400)
 
 #bouton nouvelle partie
 New= Button(invade, text='Nouvelle Partie')#, command=NewGame)
-New.place(x=1400, y= 200)
+New.place(in_=Menu, x=80, y= 370)
 
 
 invade.mainloop()
